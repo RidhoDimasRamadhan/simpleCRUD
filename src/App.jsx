a3e1a3a74b71d311f7d1b6570d509c8ff5e00c9c
Ridho Dimas Ramadhan <ridhodimasramadhan@MacBook-Pro-Ridho.local>
2024-05-13 01:48:40 +0700

    menghapus useState yg tidak penting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import CreateUser from './create'
-
 import User from './user'
 import Update from './update'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
   <BrowserRouter>
